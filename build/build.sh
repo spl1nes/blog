d9d02cfa4e9ecbc35bfe4f3c00d92af9d15f3373
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-09-03 10:25:29 +0200

    fix script

--- build/build.sh
+++ build/build.sh
@@ -9,7 +9,7 @@ BASE_PATH="/var/www/html"
 ROOT_PATH="/var/www/html/blog"
 BUILD_PATH="/var/www/html/blog/build/bin"
 
-mkdir -p ${BASE_PATH}
+mkdir -p ${BUILD_PATH}
 
 echo "#################################################"
 echo "Update blog"
