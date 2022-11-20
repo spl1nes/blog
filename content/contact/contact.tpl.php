3c476ac79c8f58ade83c7947346e84a35fa4c69d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-11-20 21:18:19 +0100

    update email

--- content/contact/contact.tpl.php
+++ content/contact/contact.tpl.php
@@ -13,7 +13,7 @@
         <h1><?= $this->data['l11n']['Contact']; ?></h1>
 
         <ul>
-            <li>Email: <a href="mailto:d.eichhorn89@web.de">d.eichhorn89@web.de</a>
+            <li>Email: <a href="mailto:info@jingga.app">info@jingga.app</a>
             <li>Github: <a href="https://github.com/Karaka-Management">https://github.com/Karaka-Management</a> and <a href="https://github.com/spl1nes">https://github.com/spl1nes</a>
             <li>Twitter: <a href="https://twitter.com/jinggaApp">https://twitter.com/jinggaApp</a>
             <li><?= $this->data['l11n']['Phone']; ?>: +49 152 04337728
